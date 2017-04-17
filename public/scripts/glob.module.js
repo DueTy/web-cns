fa960545d79afbf2f49665ca4d2815279b66e50c
DueTy <420770824@qq.com>
2017-04-17 14:32:34 +0800

    add side-close&open btn

--- public/scripts/glob.module.js
+++ public/scripts/glob.module.js
@@ -1,7 +1,18 @@
 define(function(require,exports,module){
 	require("fullPage");
+	var main_box = $(".main-box"),
+		side_close = $(".side-close"),
+		side_open = $(".side-open");
 	var side_bar_main = $(".side-bar-main"),
 		search_bar_main = $(".search-bar-main");
+
+
+	side_close.on("click", function() {		
+		main_box.addClass("page-side-close");
+	});
+	side_open.on("click", function() {
+		main_box.removeClass("page-side-close");
+	});
 
 	side_bar_main.fullHeight();
 	search_bar_main.fullHeight();
